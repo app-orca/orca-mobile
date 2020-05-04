757e3c6087bdf39f32324de3e0180022112f0dc6
DCRG18200 <dicriroga18200@gmail.com>
2020-05-04 16:25:45 -0500

    Creación carpetas Lost

--- src/pages/lost/lost.js
+++ src/pages/lost/lost.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-const Lost = () => (
-  <View>
-    <Text>Lost works!</Text>
-  </View>
-);
+import {HeaderNav, StepperView} from '../../components';
+import LostMenu from './lost-menu/lost-menu';
+import LostDescription from './lost-description/lost-description';
+
+import styles from './lost.styles';
+
+const lostOptions = [
+  {label: 'Plan alcancía', value: 'ALCANCIA'},
+  {label: 'Plan padrino', value: 'PADRINO'},
+];
+
+const Lost = ({navigation}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedLost, setSelectedLost] = useState(lostOptions[0]);
+
+  const handleGoHomePress = () => navigation.popToTop();
+
+  const handleGoBack = () => setCurrentIndex(currentIndex - 1);
+
+  const handleOptionPress = value => {
+    setSelectedLost(lostOptions.find(lost => lost.value === value));
+    setCurrentIndex(1);
+  };
+
+  const handleLostPress = () => setCurrentIndex(2);
+
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <HeaderNav
+        onBackPress={currentIndex > 0 ? handleGoBack : null}
+        onGoHomePress={handleGoHomePress}
+        title="Perdidos"
+      />
+      <StepperView style={styles.wrapper} {...{currentIndex}}>
+        <LostMenu onOptionPress={handleOptionPress} options={lostOptions} />
+        <LostDescription onLostPress={handleLostPress} lost={selectedLost} />
+      </StepperView>
+    </SafeAreaView>
+  );
+};
 
 export default Lost;
