86fe625714edc8f35dabd800dbb3902ae5f4e74f
Miguel Palacio <migpalg@gmail.com>
2020-05-15 03:19:47 -0500

    Updated donations text

--- src/pages/donations/donation-description/donation-description.js
+++ src/pages/donations/donation-description/donation-description.js
@@ -7,17 +7,13 @@ import propTypes from 'prop-types';
 
 import DonateButtonImage from 'assets/images/buttons/donate-btn.svg';
 
-const MOCK_TEXT =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula felis sed enim mollis, a eleifend arcu placerat. Vivamus consectetur a lorem id sagittis. Donec vel pellentesque urna. Aliquam lacus massa, sollicitudin a libero sit amet, blandit fermentum est. In cursus posuere odio, sit amet cursus sem venenatis vitae. Etiam eu cursus magna, ut consectetur tellus. In eu mauris quis ante luctus cursus in at leo. Maecenas a bibendum nunc. Pellentesque tempor velit ut eros tempus, nec hendrerit magna vestibulum. Nunc eget lectus non velit vestibulum ultricies. Donec faucibus ac sem ut aliquet. \n' +
-  '\nPraesent pellentesque neque non pulvinar venenatis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum enim lectus, imperdiet a odio vel, lobortis porta tellus. Vivamus sed lorem gravida, cursus risus nec, fermentum odio. Etiam posuere, nibh in pulvinar varius, sapien magna iaculis velit, id auctor sem neque vitae mauris. Nam tempor eu sapien quis rutrum. Nullam feugiat commodo bibendum. Maecenas vulputate pellentesque orci. Pellentesque at vehicula lacus. Duis viverra felis at lorem pellentesque molestie. Etiam sit amet pulvinar ante. Ut consectetur porttitor tortor id tincidunt. Duis at scelerisque augue. Nullam ut volutpat lorem.';
-
 const DonationDescription = ({donation, onDonatePress}) => (
   <View style={styles.wrapper}>
     <ScrollView style={styles.descriptionContainer}>
       <Text style={styles.descriptionTitle} h2>
         {donation.label}
       </Text>
-      <Text style={styles.descriptionText}>{MOCK_TEXT}</Text>
+      <Text style={styles.descriptionText}>{donation.text}</Text>
     </ScrollView>
     <TouchableOpacity onPress={onDonatePress}>
       <DonateButtonImage width="60%" style={styles.donateButton} />
@@ -29,6 +25,7 @@ DonationDescription.propTypes = {
   donation: propTypes.shape({
     label: propTypes.string.isRequired,
     value: propTypes.string.isRequired,
+    text: propTypes.string,
   }),
   onDonatePress: propTypes.func,
 };
--- src/pages/donations/donations.js
+++ src/pages/donations/donations.js
@@ -9,10 +9,30 @@ import Donate from './donate/donate';
 import styles from './donations.styles';
 
 const donationOptions = [
-  {label: 'Plan alcancía', value: 'ALCANCIA'},
-  {label: 'Plan padrino', value: 'PADRINO'},
-  {label: 'Donación Única', value: 'UNICA'},
-  {label: 'Donación empresarial', value: 'EMPRESARIAL'},
+  {
+    label: 'Plan alcancía',
+    value: 'ALCANCIA',
+    text:
+      'Consiste en un compromiso que hacen los donantes para donar una cuota en dinero fija (5.000-10.000-20.000), cada mes o con la frecuencia que les sea posible y de acuerdo a sus posibilidades económicas. El dinero obtenido de estas donaciones se destina a la manutención de los animales y a suplir las necesidades principales de los animales rescatados, como la alimentación y las atenciones veterinarias.',
+  },
+  {
+    label: 'Plan padrino',
+    value: 'PADRINO',
+    text:
+      'Padrino, es aquella persona que se encarga de velar y proteger a otro, así, hay varios peludos que necesitan de un mano amiga y sentirse aquel ser importante en el corazón de otro, pues somos unos eternos convencidos que el amor, todo lo puede. Los padrinos, le devuelven a un ser abandonado, la certeza de ser un alguien importante, una vida llena de oportunidades; la posibilidad de olvidar, los padecimientos del pasado, el dolor, el hambre, el frío y el miedo, sabiendo que hay alguien que los apoya, que los quiere y los acompaña hasta que logren el fin de hallar un lindo hogar. El padrino hace una inscripción y se encarga de hacer un acompañamiento de su ahijado, dotándolo de una cuota, por medio de la cual, garantizamos su alimentación y atenciones veterinarias; dicha cuota es flexible y permite, brindarle al peludo escogido, todo aquello que requiere. Además, el padrino, es el encargado de hacer seguimiento tras su proceso de adopción, y mientras encuentra su hogar, es quien le dona todo el amor, afecto y cariño que el necesita.',
+  },
+  {
+    label: 'Donación Única',
+    value: 'UNICA',
+    text:
+      'O.R.C.A. es una Organización sin ánimo de lucro, en ningún momento se generan ingresos para los miembros de la fundación, antes por el contrario, el mayor porcentaje de los mismos, deben aportar una cuota mensual para el mantenimiento de la institución y para poder cancelar los gastos que se derivan de las atenciones de los perros y gatos. El 80% de la financiación de O.R.C.A. proviene de las donaciones que las personas que conocen la labor de la institución, otorgan a la misma; ello, porque las cuentas de lo atendido en los diferentes centros veterinarios (como el Centro Veterinario El Poblado), son expuestas para la revisión y conocimiento de todos los integrantes. A la fecha, O.R.C.A. adeuda al CVP una suma de aproximadamente $18.000.000 (dieciocho millones de pesos).',
+  },
+  {
+    label: 'Donación empresarial',
+    value: 'EMPRESARIAL',
+    text:
+      'Cuando una compañía adquiere el compromiso de donar no solo contribuye a una causa social sino que también se puede aprovechar de determinados beneficios tributarios que se establecen por ley. La entidad beneficiaria de (Fundación O.R.C.A) la donación está calificada en el Régimen Tributario Especial, está legalmente constituida y está sometida a inspección, control y vigilancia de una entidad estatal.',
+  },
 ];
 
 const Donations = ({navigation}) => {
